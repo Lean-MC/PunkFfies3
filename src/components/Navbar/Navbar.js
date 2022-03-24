8f309c1c7281dd37a56ee7b0be02a4dcb57716ca
Your Name01~ <leanmaciel91@gmail.com>
2022-03-24 17:39:19 -0300

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+ return(
+     <nav className="Navbar">
+         <ul>
+             <li><h1>Punkffies</h1></li>
+             <li><button>Punkffies</button></li>
+             <li><button>Punkcakes</button></li>
+             <li><button>Punktres</button></li>
+         </ul>
+
+     </nav>
+)
+}
+
+export default Navbar
